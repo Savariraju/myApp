9216200814d65d8b04f1cf3519ebd9ec5bd027b1
Savariraju <43338041+Savariraju@users.noreply.github.com>
2023-05-22 15:48:49 +0530

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { timer } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'myApp';
+  public timerInfo: any = [];
+  ngOnInit() {
+    const timer$ = timer(1000, 1000);
+    let i = 0;
+    timer$.subscribe(() => {
+      this.timerInfo.push({
+        title: i,
+      });
+      i++;
+    });
+  }
 }
